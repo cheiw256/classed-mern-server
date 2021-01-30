87a1a08e93944bc29344bd562e747924e634bce8
cheiw256 <chiew256@gmail.com>
2021-01-30 17:45:56 +0800

    init

---
+++ routes/users.js
@@ -0,0 +1,23 @@
+"use strict";
+const express = require('express');
+let router = express.Router();
+let User = require('../models/user.model');
+
+
+router.get('/',(req,res) =>{
+    User.find()
+    .then(users => res.json(users))
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.post('/add',(req,res) =>{
+    const username = req.body.username;
+
+    const newUser = new User({username});
+  
+    newUser.save()
+      .then(() => res.json('User added!'))
+      .catch(err => res.status(400).json('Error: ' + err));
+});
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,34 @@
+"use strict";
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+require('dotenv').config();
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, 
+    {useNewUrlParser:true,
+    useCreateIndex:true,
+    useUnifiedTopology:true});
+
+const connection = mongoose.connection;
+connection.once('open', (err) =>{
+    if(err) console.error(err);
+    else console.log('MongoDB database connection established successfully');
+})
+
+const exerciseRouter = require('./routes/exercises');
+const userRouter = require('./routes/users');
+
+app.use('/exercises', exerciseRouter);
+app.use('/users', userRouter);
+
+
+app.listen(port, () =>{
+    console.log(`Server is running on port ${port}`);
+})
+
